3e0226752601a1eaf6164ca8a103812440525095
laserspike33 <164445976+laserspike33@users.noreply.github.com>
2025-08-22 11:21:50 +1000

    Add files via upload

---
+++ BuggyProgram.py
@@ -0,0 +1,12 @@
+# Sample buggy Python program
+from datetime import datetime
+current_time = datetime.now()
+ 
+def copyFileDirectory(name):
+    print("Hello, {name}")     
+ 
+def sendEmail():
+ print("Sending email")
+ copyFileDirectory("Alice")            
+ 
+arg1 = "testing"
